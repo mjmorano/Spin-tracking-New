9794f5152b4d29599fa222274c14c6483a1bd2a4
mjmorano <mjmorano@ncsu.edu>
2023-01-18 08:43:26 -0500

    Fixed problem with call signature

--- include/DOP853func.h
+++ include/DOP853func.h
@@ -7,15 +7,15 @@ void obs(long nr, double xold, double x, double* y, int* irtrn);
 
 double pulse(const double t);
 
-void Bloch(const double t, const double* y, double* f, const double B0, const double gamma, const double t0, const double tf ,const double* x_old, const double* x_new, const double* v_old, const double* v_new);
+void Bloch(const double t, const double* y, double* f, const double B0, const double gamma, const double t0, const double tf ,const double* p_old, const double* p_new, const double* v_old, const double* v_new);
 
-void interpolate(const double t, const double t0, const double tf, const double* x_old, const double* x_new, const double* v_old, const double* v_new, double* p_out, double* v_out);
+void interpolate(const double t, const double t0, const double tf, const double* p_old, const double* p_new, const double* v_old, const double* v_new, double* p_out, double* v_out);
 
 void grad(double*, double*);
 
 // double hinit(double, double*, double, double*, double*, double*, int, double, double, double);
 
-int integrate(double t0, double tf, double* y0, const double* x_old, const double* x_new, const double* v_old, const double* v_new, options OPT);
+int integrate(double t0, double tf, double* y0, const double* p_old, const double* p_new, const double* v_old, const double* v_new, options OPT);
 
 double sign(double, double);
 
