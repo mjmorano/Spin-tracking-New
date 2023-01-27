a8c03570656210b05cb7ca341130c4d70929c349
mjmorano <mjmorano@ncsu.edu>
2023-01-27 12:00:10 -0500

    Added the timing back in

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <ctime>
+#include <chrono>
 #include "include/particle.h"
 #include "include/options.h"
 #include <openacc.h>
 
 using namespace std;
+using namespace std::chrono;
 
 int main(int argc, char* argv[]) {
 
@@ -19,11 +21,15 @@ int main(int argc, char* argv[]) {
 	
 	outputDtype * outputArray = (outputDtype*)malloc(outputSize*numParticles);
 
+	auto start = high_resolution_clock::now();
 	#pragma acc parallel loop
 	for(unsigned int n = 0; n<numParticles;n++){
 		particle p(yi, n+timestamp, opt, &outputArray[n*numOutput]);
 		p.run();
 	}
+	auto end = high_resolution_clock::now();
+	auto duration = duration_cast<milliseconds>(end-start);
+	printf("%d\n", duration);
 		
 	FILE* f = fopen(outputFilename, "wb");
 	fwrite(outputArray, outputSize * numParticles, 1, f);
